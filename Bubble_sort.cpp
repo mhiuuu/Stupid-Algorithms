447507819f8de59535597aa3a1da5f4a7b8fa5e6
Mai Trung Hieu (Mike) <95751244+mhiuuu@users.noreply.github.com>
2022-08-12 22:29:09 +0700

    I forgot curly braces....

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -13,8 +13,10 @@ Disadvantages:
 - Not fast enough for large data.
 */
 //Code:
-for (int i = 0; i < n; i++)
-	for (int j = 0; j < n - 1; j++)
+for (int i = 0; i < n; i++) {
+	for (int j = 0; j < n - 1; j++) {
 		if (a[j] > a[j+1]) {
 			swap(a[j], a[j+1]);
 		}
+	}
+}
